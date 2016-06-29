2701e5d430c2c0f8da47d3ef1e4790a0e1ad78fb
Jordan Neufeld <myjc.niv@gmail.com>
2016-06-29 00:20:41 -0500

    stuffs

--- index.js
+++ index.js
@@ -1,10 +1,73 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+var verificationToken = process.env.SLACK_VERIFICATION_TOKEN || 'foobar';
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
+//Slack token authentication middleware
+app.use(function (req, res, next) {
+  if (req.body.token == verificationToken){
+    console.log(JSON.stringify(req.body));
+    next();
+  } else {
+    console.log('Failed token verification.');
+    console.log('Expected token: '+verificationToken)
+    console.log('Received token: '+req.body.token);
+  }
+});
+
+app.post('/secret', function (req, res) {
+  res.json({
+    "text": "New comic book alert!",
+    "attachments": [
+        {
+            "title": "The Further Adventures of Slackbot",
+            "fields": [
+                {
+                    "title": "Volume",
+                    "value": "1",
+                    "short": true
+                },
+                {
+                    "title": "Issue",
+                    "value": "3",
+            "short": true
+                }
+            ],
+            "author_name": "Stanford S. Strickland",
+            "author_icon": "https://api.slack.com/img/api/homepage_custom_integrations-2x.png",
+            "image_url": "http://i.imgur.com/OJkaVOI.jpg?1"
+        },
+        {
+            "title": "Synopsis",
+            "text": "After @episod pushed exciting changes to a devious new branch back in Issue 1, Slackbot notifies @don about an unexpected deploy..."
+        },
+        {
+            "fallback": "Would you recommend it to customers?",
+            "title": "Would you recommend it to customers?",
+            "callback_id": "comic_1234_xyz",
+            "color": "#3AA3E3",
+            "attachment_type": "default",
+            "actions": [
+                {
+                    "name": "recommend",
+                    "text": "Recommend",
+                    "type": "button",
+                    "value": "recommend"
+                },
+                {
+                    "name": "no",
+                    "text": "No",
+                    "type": "button",
+                    "value": "bad"
+                }
+            ]
+        }
+    ]
+});
 });
 
 app.listen(process.env.PORT, function () {
-  console.log('Example app listening!');
+  console.log('Example app listening');
 });
