ed4bbc7e136d765f316900ac8339b59ea2f70ce2
Jordan Neufeld <myjc.niv@gmail.com>
2016-06-28 23:23:14 -0500

    fix port

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express();
-
+var port = process.env.PORT || 3000;
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(process.env.PORT, function () {
+  console.log('Example app listening!');
 });
