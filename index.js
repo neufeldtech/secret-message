8047952fa73e8355af2790a4ed27ed71c404f339
Jordan Neufeld <myjc.niv@gmail.com>
2016-06-30 10:05:41 -0500

    fix message

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 var verificationToken = process.env.SLACK_VERIFICATION_TOKEN || 'foobar';
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}));
-
 var clientID = process.env.SLACK_CLIENT_ID || 'foobar';
 var clientSecret = process.env.SLACK_CLIENT_SECRET || 'foobar';
 var callbackURL = process.env.SLACK_CALLBACK_URL || 'foobar';
-
+var bodyParser = require('body-parser');
 var passport = require('passport');
 var SlackStrategy = require('passport-slack').Strategy;
+
+app.use(bodyParser.urlencoded({extended: true}));
 passport.use(new SlackStrategy({
         clientID: clientID,
         clientSecret: clientSecret,
@@ -30,69 +29,47 @@ app.get('/',function (req, res){
   res.json({"message":"OK"})
 })
 
-app.use(function (req, res, next) {
-  console.log(JSON.stringify(req.body));
-  if (req.body.token == verificationToken){
-    next();
+app.post('/secret', function (req, res) {
+  console.log(req)
+  var body = req.body
+  if (body.token == verificationToken){
+    res.json({
+      "text": body.user_name + "Sent a secret message",
+      "attachments": [
+        {
+          "title": "Would you recommend it to customers?",
+          "callback_id": "readMessage",
+          "color": "#3AA3E3",
+          "attachment_type": "default",
+          "actions": [
+            {
+              "name": "readMessage",
+              "text": "View message",
+              "type": "button",
+              "value": "readMessage",
+              "confirm": {
+                "title": "This message will self destruct after reading!",
+                "text": body.text,
+                "ok_text":"I have read the message!",
+                "dismiss_text":"Cancel"
+              }
+            }
+          ]
+        }
+      ]
+    });//end res.json
   } else {
     console.log('Failed token verification.');
     console.log('Expected token: '+verificationToken)
     console.log('Received token: '+req.body.token);
+    return
   }
 });
 
-app.post('/secret', function (req, res) {
-  console.log(req)
-  res.json({
-    "text": "New comic book alert!",
-    "attachments": [
-        {
-            "title": "The Further Adventures of Slackbot",
-            "fields": [
-                {
-                    "title": "Volume",
-                    "value": "1",
-                    "short": true
-                },
-                {
-                    "title": "Issue",
-                    "value": "3",
-            "short": true
-                }
-            ],
-            "author_name": "Stanford S. Strickland",
-            "author_icon": "https://api.slack.com/img/api/homepage_custom_integrations-2x.png",
-            "image_url": "http://i.imgur.com/OJkaVOI.jpg?1"
-        },
-        {
-            "title": "Synopsis",
-            "text": "After @episod pushed exciting changes to a devious new branch back in Issue 1, Slackbot notifies @don about an unexpected deploy..."
-        },
-        {
-            "fallback": "Would you recommend it to customers?",
-            "title": "Would you recommend it to customers?",
-            "callback_id": "comic_1234_xyz",
-            "color": "#3AA3E3",
-            "attachment_type": "default",
-            "actions": [
-                {
-                    "name": "recommend",
-                    "text": "Recommend",
-                    "type": "button",
-                    "value": "recommend"
-                },
-                {
-                    "name": "no",
-                    "text": "No",
-                    "type": "button",
-                    "value": "bad"
-                }
-            ]
-        }
-    ]
-});
-});
+app.post('/update', function (req, res) {
+  console.log(JSON.stringify(req.body));
 
+});
 app.listen(process.env.PORT, function () {
   console.log('Example app listening');
 });
