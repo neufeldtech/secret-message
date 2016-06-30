52bc2a1aa9eec7637d6897d2fce347bfdd96fb1b
Jordan Neufeld <myjc.niv@gmail.com>
2016-06-30 09:46:43 -0500

    oauth poc working

--- index.js
+++ index.js
@@ -5,6 +5,26 @@ var verificationToken = process.env.SLACK_VERIFICATION_TOKEN || 'foobar';
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 
+var clientID = process.env.SLACK_CLIENT_ID || 'foobar';
+var clientSecret = process.env.SLACK_CLIENT_SECRET || 'foobar';
+var callbackURL = process.env.SLACK_CALLBACK_URL || 'foobar';
+
+var passport = require('passport');
+var SlackStrategy = require('passport-slack').Strategy;
+passport.use(new SlackStrategy({
+        clientID: clientID,
+        clientSecret: clientSecret,
+        callbackURL: callbackURL,
+        scope: 'commands',
+        skipUserProfile: true
+    }, ()=>{}));
+
+app.get('/auth/slack', passport.authenticate('slack'));
+
+app.get('/auth/slack/callback', function(req, res) {
+    res.redirect('https://my.slack.com');
+  });
+
 //Slack token authentication middleware
 app.get('/',function (req, res){
   res.json({"message":"OK"})
@@ -22,6 +42,7 @@ app.use(function (req, res, next) {
 });
 
 app.post('/secret', function (req, res) {
+  console.log(req)
   res.json({
     "text": "New comic book alert!",
     "attachments": [
