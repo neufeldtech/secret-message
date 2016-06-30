e427c3614f846ebd1c6c926311c34a434f61e787
Jordan Neufeld <myjc.niv@gmail.com>
2016-06-30 10:40:04 -0500

    change response type and respond later

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var verificationToken = process.env.SLACK_VERIFICATION_TOKEN || 'foobar';
 var clientID = process.env.SLACK_CLIENT_ID || 'foobar';
 var clientSecret = process.env.SLACK_CLIENT_SECRET || 'foobar';
 var callbackURL = process.env.SLACK_CALLBACK_URL || 'foobar';
+var request = require('request');
 var bodyParser = require('body-parser');
 var passport = require('passport');
 var SlackStrategy = require('passport-slack').Strategy;
@@ -14,7 +15,7 @@ passport.use(new SlackStrategy({
         clientID: clientID,
         clientSecret: clientSecret,
         callbackURL: callbackURL,
-        scope: 'commands',
+        scope: 'commands chat:write:bot',
         skipUserProfile: true
     }, ()=>{}));
 
@@ -28,36 +29,57 @@ app.get('/auth/slack/callback', function(req, res) {
 app.get('/',function (req, res){
   res.json({"message":"OK"})
 })
+function sendSecret(responseUrl, username, text){
+  var message = {
+    "response_type":"in_channel",
+    "attachments": [
+      {
+        "title": username + " sent a secret message:",
+        "callback_id": "readMessage",
+        "color": "#3AA3E3",
+        "attachment_type": "default",
+        "actions": [
+          {
+            "name": "readMessage",
+            "text": "View message",
+            "type": "button",
+            "value": "readMessage",
+            "confirm": {
+              "title": "This message will self destruct after reading!",
+              "text": text,
+              "ok_text":"I have read the message!",
+              "dismiss_text":"Cancel"
+            }
+          }
+        ]
+      }
+    ]
+  }
+  request(
+    {
+      method: 'post',
+      uri: responseUrl,
+      json: true,
+      body: message
+    }
+  , function (error, response, body) {
+      if(response.statusCode == 200){
+        return
+      } else {
+        console.log('error: '+ response.statusCode)
+        console.log(body)
+      }
+    }
+  );
+}
 
 app.post('/secret', function (req, res) {
   console.log(req)
   var body = req.body
   if (body.token == verificationToken){
-    res.json({
-      "response_type":"in_channel",
-      "attachments": [
-        {
-          "title": body.user_name + " sent a secret message:",
-          "callback_id": "readMessage",
-          "color": "#3AA3E3",
-          "attachment_type": "default",
-          "actions": [
-            {
-              "name": "readMessage",
-              "text": "View message",
-              "type": "button",
-              "value": "readMessage",
-              "confirm": {
-                "title": "This message will self destruct after reading!",
-                "text": body.text,
-                "ok_text":"I have read the message!",
-                "dismiss_text":"Cancel"
-              }
-            }
-          ]
-        }
-      ]
-    });//end res.json
+    res.end(null,function(err){
+      sendSecret(body.responseUrl, body.user_name, body.text);
+    });
   } else {
     console.log('Failed token verification.');
     console.log('Expected token: '+verificationToken)
