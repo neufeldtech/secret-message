48581185c54b1260d8dd64a00780db1188d068b3
Jordan Neufeld <myjc.niv@gmail.com>
2016-07-04 23:49:48 -0500

    started redis tests

--- redisService.js
+++ redisService.js
@@ -1,3 +1,23 @@
-module.exports = function(){
-  
+var shortId = require('shortid');
+module.exports = function(client){
+  var module = {};
+
+    client.on('connect', function (err) {
+      console.log('redis client connected')
+    });
+
+    client.on('error', function(err){
+      console.log('Redis error: '+err)
+    })
+
+  module.set = function(key, value, callback){
+    client.set(key, value, function(err,reply){
+      if (err){
+        callback("Error setting redis key: "+err)
+      } else {
+        callback(null, reply)
+      }
+    })
+  }
+  return module;
 }
--- test/redisService.test.js
+++ test/redisService.test.js
@@ -1,3 +1,12 @@
 var should = require('chai').should();
-var client = 'i am the redis client'
-var redis = require('../redisService.js')(client)
+var redis = require('redis-mock')
+var redisService = require('../redisService.js')(redis.createClient())
+
+describe("set key in redis", function(){
+  it("should return 'OK' if successfull", function(done){
+    redisService.set('yolo','testValue',function(err,reply){
+      reply.should.equal('OK');
+      done();
+    })
+  })
+})
